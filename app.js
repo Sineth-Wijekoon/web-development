dbdf10ddbf4b8eec6cbe0d0f9311b8a8345a5428
Sineth-Wijekoon <wijekoonsineth@gmail.com>
2025-02-25 19:52:08 +0530

    Homepage Modifications

--- app.js
+++ app.js
@@ -43,3 +43,20 @@ let lastScrollTop = 0;
     lastScrollTop = scrollTop;
 });
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("homepage_image");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000);
+}
+
